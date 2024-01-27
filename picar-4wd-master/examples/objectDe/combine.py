c457af2302972a140856f76a76148eab3589d39b
Bryan1203 <bryanchang1203@hotmail>
2024-01-27 12:02:37 -0600

    a

--- picar-4wd-master/examples/objectDe/combine.py
+++ picar-4wd-master/examples/objectDe/combine.py
@@ -3,7 +3,7 @@ import object_detect
 import keyboard_control
 
 t1 = threading.Thread(target=object_detect.main())
-t2 = threading.Thread(target=keyboard_control.main())
+t2 = threading.Thread(target=keyboard_control.Keyborad_control())
 
 t1.start()
 t2.start()
