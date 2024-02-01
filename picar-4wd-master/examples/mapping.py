0cfd9b0c6db9cbbdd54fb8b25eef2a9117fc48d6
Bryan1203 <bryanchang1203@hotmail>
2024-01-31 20:28:12 -0600

    added A*

--- picar-4wd-master/examples/mapping.py
+++ picar-4wd-master/examples/mapping.py
@@ -31,9 +31,11 @@ def padPointMap(arr):
                 if len(group) > 3:
                     # Pad two 1s before and after the group
                     if group[0] > 1:
-                        arr_copy[i, group[0] - 1] = 2
+                        arr_copy[i, group[0] - 1] = 3
+                        arr_copy[i, group[0] - 2] = 3
                     if group[-1] < len(row) - 1:
-                        arr_copy[i, group[-1] + 1] = 2
+                        arr_copy[i, group[-1] + 1] = 3
+                        arr_copy[i, group[-1] + 2] = 3
 
         return arr_copy
 
